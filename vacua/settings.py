83d76ccc922e3cdb2ed8705562a909f47cbae49b
LL-Etiane <lukongleinyuyetiane@gmail.com>
2023-03-27 03:59:44 +0100

    feat: app dockerized

--- vacua/settings.py
+++ vacua/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'NAME': env("DB_NAME"),
         'USER': env("DB_USER"),
         'PASSWORD': env("DB_PASSWORD"),
-        'HOST': env("BD_HOST"),
+        'HOST': env("DB_HOST"),
         'PORT': env("DB_PORT")
     }
 }
