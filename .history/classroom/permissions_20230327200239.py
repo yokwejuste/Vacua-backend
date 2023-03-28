32139fbaa8ace1f1cbd50f669562869af06edbe6
Steve Yonkeu <yokwejuste@yahoo.com>
2023-03-28 14:42:59 +0100

    updates: updates the code base with the predata

---
+++ .history/classroom/permissions_20230327200239.py
@@ -0,0 +1,36 @@
+from rest_framework import permissions
+from rest_framework.permissions import BasePermission
+
+
+class IsHOD(BasePermission):
+    def has_object_permission(self, request, view, obj) -> bool:
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        return obj.is_hod == request.user.is_hod
+
+
+class IsAdmin(BasePermission):
+    def has_object_permission(self, request, view, obj) -> bool:
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        return obj.is_admin == request.user.is_admin
+
+
+class IsClassCoordinator(BasePermission):
+    def has_object_permission(self, request, view, obj) -> bool:
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        return obj.is_cc == request.user.is_cc
+    
+    def has_permission(self, request, view)
+
+
+class IsDirector(BasePermission):
+    def has_object_permission(self, request, view, obj) -> bool:
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        return obj.is_director == request.user.is_director
---
+++ pre_data/data.py
@@ -0,0 +1,91 @@
+SCHOOLS = [
+    {
+        'name': 'School of Computing',
+        'director': 'Dr. John Doe'
+    },
+    {
+        'name': 'School of Engineering',
+        'director': 'Dr. Alexia Doe'
+    },
+    {
+        'name': 'School of Business',
+        'director': 'Dr. John Booker'
+    },
+    {
+        'name': 'School of Law',
+        'director': 'Dr. Alfred Doe'
+    },
+    {
+        'name': 'School of Medicine',
+        'director': 'Dr. Boris John'
+    },
+    {
+        'name': 'School of Arts',
+        'director': 'Dr. John Doe'
+    },
+    {
+        'name': 'School of Science',
+        'director': 'Dr. John Doe'
+    },
+    {
+        'name': 'School of Education',
+        'director': 'Dr. John Doe'
+    },
+    {
+        'name': 'School of Agriculture',
+        'director': 'Dr. John Doe'
+    },
+    {
+        'name': 'School of Social Sciences',
+        'director': 'Dr. John Doe'
+    },
+    {
+        'name': 'School of Humanities',
+        'director': 'Dr. John Doe'
+    },
+    {
+        'name': 'School of Languages',
+        'director': 'Dr. John Doe'
+    },
+    {
+        'name': 'School of Music',
+        'director': 'Dr. John Doe'
+    },
+    {
+        'name': 'School of Journalism',
+        'director': 'Dr. John Doe'
+    },
+]
+
+DEPARTMENT = [
+    {
+        "name": "Computer Science",
+        "school": "School of Science",
+        "hod": "Dr. Catherine Ologwu"
+    },
+    {
+        "name": "Mathematics",
+        "school": "School of Science",
+        "hod": "Dr. Alphonse Ogunlana"
+    },
+    {
+        "name": "Physics",
+        "school": "School of Science",
+        "hod": "Dr. Martins James"
+    },
+    {
+        "name": "Chemistry",
+        "school": "School of Science",
+        "hod": "Dr. Oluwaseun Ogunlana"
+    },
+    {
+        "name": "Biology",
+        "school": "School of Science",
+        "hod": "Dr. Oluwaseun Ogunlana"
+    },
+    {
+        "name": "Geology",
+        "school": "School of Science",
+        "hod": "Dr. Oluwaseun Ogunlana"
+    },
+]
