01f68ccb404c0c6c2ef66be807dafbfb1a1fa157
Steve Yonkeu <yokwejuste@yahoo.com>
2023-03-29 09:07:22 +0100

    feat: adds more featues to the auth serializer

--- classroom/views/auth_views.py
+++ classroom/views/auth_views.py
@@ -8,7 +8,8 @@ from oauth2_provider.contrib.rest_framework import OAuth2Authentication
 from oauth2_provider.models import Application, AccessToken
 from oauth2_provider.views import TokenView
 from rest_framework import generics, status
-from rest_framework.permissions import AllowAny
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import AllowAny, IsAdminUser, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -16,10 +17,13 @@ from classroom.serializers.auth_serilizers import *
 
 
 class RegistrationView(generics.GenericAPIView):
+    authentication_classes = [BasicAuthentication, ]
     serializer_class = RegistrationSerializer
-    permission_classes = [AllowAny, ]
+    permission_classes = [IsAdminUser, ]
 
     @swagger_auto_schema(
+        operation_id="register",
+        operation_summary="Register a new user",
         operation_description="Register a new user",
         request_body=RegistrationSerializer,
         responses={
@@ -51,6 +55,8 @@ class LoginView(APIView, TokenView):
         return self.serializer_class(*args, **kwargs)
 
     @swagger_auto_schema(
+        operation_id="Login",
+        operation_summary="Login a user",
         operation_description="Login a user",
         request_body=LoginSerializer,
         responses={
@@ -98,6 +104,70 @@ class LoginView(APIView, TokenView):
             }
         }
         return Response(response_data, status=status.HTTP_200_OK)
+
+
+class UpdateUserView(generics.GenericAPIView):
+    authentication_classes = [OAuth2Authentication, ]
+    serializer_class = UpdateUserSerializer
+    permission_classes = [AllowAny, ]
+
+    @swagger_auto_schema(
+        operation_id="Update User",
+        operation_summary="Update a user",
+        operation_description="Update a user",
+        request_body=UpdateUserSerializer,
+        responses={
+            200: UpdateUserSerializerResponse(),
+            300: "Multiple choices",
+            400: "Bad request",
+            500: "Internal server error",
+        }
+    )
+    def patch(self, request):
+        user = request.user
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        data = serializer.validated_data
+        user.first_name = data.get('first_name')
+        user.last_name = data.get('last_name')
+        user.email = data.get('email')
+        user.save()
+        response_data = {
+            "user": {
+                "id": user.id,
+                "email": user.email,
+                "full_name": f"{user.first_name} {user.last_name}",
+                "user_type": str(user.get_user_role())
+            }
+        }
+        return Response(response_data, status=status.HTTP_200_OK)
+
+
+class ChangePasswordView(generics.GenericAPIView):
+    authentication_classes = [OAuth2Authentication, ]
+    serializer_class = ChangePasswordSerializer
+    permission_classes = [IsAuthenticated, ]
+
+    @swagger_auto_schema(
+        operation_id="Change Password",
+        operation_summary="Change a user's password",
+        operation_description="Change a user password",
+        request_body=ChangePasswordSerializer,
+        responses={
+            200: "Password changed successfully",
+            300: "Multiple choices",
+            400: "Bad request",
+            500: "Internal server error",
+        }
+    )
+    def patch(self, request):
+        user = request.user
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        data = serializer.validated_data
+        user.set_password(data.get('new_password'))
+        user.save()
+        return Response({"message": "Password changed successfully"}, status=status.HTTP_200_OK)
 
 
 class LogoutView(generics.GenericAPIView):
