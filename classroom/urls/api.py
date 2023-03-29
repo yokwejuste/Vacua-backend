ee8bbc74e3a8d34da5b323cd86d22b2d29fd1d1c
Steve Yonkeu <yokwejuste@yahoo.com>
2023-03-29 16:40:29 +0100

    refactor: modularization of the auth

--- classroom/urls/api.py
+++ classroom/urls/api.py
@@ -1,9 +1,22 @@
 from django.urls import re_path
 
-from classroom.views.auth_views import RegistrationView, LoginView, LogoutView
+from classroom.views.auth_views import *
 
 urlpatterns = [
-    re_path(r'auth/register', RegistrationView.as_view(), name='register'),
+    # common paths
+    re_path(r'auth/change_password', ChangePasswordView.as_view(), name='change_password'),
     re_path(r'auth/login', LoginView.as_view(), name='login'),
     re_path(r'auth/logout', LogoutView.as_view(), name='logout'),
+
+    # Class coordinator authentication urls
+    re_path(r'auth/cc/register', CCRegistrationView.as_view(), name='cc_register'),
+    re_path(r'auth/cc/update', CCUpdateUserView.as_view(), name='cc_update'),
+
+    # Head of department authentication urls
+    re_path(r'auth/hod/register', HodRegistrationView.as_view(), name='hod_register'),
+    re_path(r'auth/hod/update', HodUpdateUserView.as_view(), name='hod_update'),
+
+    # Director of school authentication urls
+    re_path(r'auth/dean/register', DeanRegistrationView.as_view(), name='dean_register'),
+    re_path(r'auth/dean/update', DeanUpdateUserView.as_view(), name='dean_update'),
 ]
